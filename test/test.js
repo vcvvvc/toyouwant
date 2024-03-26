fa7c8b8eebb109061d620625679e068c1e4d6dd2
Vc <vcvckw@gmail.com>
2024-03-27 01:46:12 +0800

    add

--- test/test.js
+++ test/test.js
@@ -14,3 +14,13 @@ console.log(text)
 //toSha256
 text = toWantstr.toSha256("Hello Wrold!");
 console.log(text)
+
+//toMd5
+text = toWantstr.toMd5("Hello World!");
+console.log(text)
+
+text = toWantstr.encryptedStr("Hello World!")
+console.log(text)
+
+text = toWantstr.toTimestamp("2022-06-15")
+console.log(text)
