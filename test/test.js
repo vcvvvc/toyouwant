06f0291d1ce8615ecebd961b9c5e586fefb5ce3c
Vc <vcvckw@gmail.com>
2024-03-27 01:39:39 +0800

    add

---
+++ test/test.js
@@ -0,0 +1,16 @@
+// const toWantstr = require('./index.js');
+const toWantstr = require('@vcvckw/towantstr');
+
+console.log("Hello Wrold!")
+
+// removeMySpaces
+text = toWantstr.removeSpace("Hello Wrold!");
+console.log(text)
+
+//toLow
+text = toWantstr.toLowText("Hello Wrold!");
+console.log(text)
+
+//toSha256
+text = toWantstr.toSha256("Hello Wrold!");
+console.log(text)
